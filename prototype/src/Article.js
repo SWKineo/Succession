c51cc80ad5dac7802abe94ddb6763499bc434293
Spencer Ward <spencer.ward@maine.edu>
2020-07-29 23:33:55 -0400

    Adding early, broken expansion

--- prototype/src/Article.js
+++ prototype/src/Article.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import ArticleAttempt from './Types/article.js';
+import { Link } from 'react-router-dom'
+import queryString from 'query-string'
+import ArticleAttempt from './Types/article.js'
 
 let sampleArticle = new ArticleAttempt()
 
@@ -19,7 +21,37 @@ export default class Article extends Component {
                 <p>{sampleArticle.formalism}</p>
                 <p>{sampleArticle.metaphor}</p>
                 {sampleArticle.body.map((chunk, index, _) => {
-                    return <p key={index}>{chunk.text}</p>
+                    let expanded = queryString.parse(this.props.location.search)
+
+                    if (chunk.expansion === undefined) {
+                        return <p key={index}>{chunk.text}</p>
+                    }
+
+                    if (expanded.index && (expanded === index || expanded.index.includes(index))) {
+                        return (
+                            <div>
+                                <p>{chunk.expansion}</p>
+                            </div>
+                        )
+                    }
+                    
+                    console.log(queryString.stringify({index}))
+                    console.log(Object.keys(expanded).length === 0)
+                    let search = ""
+                    if (Object.keys(expanded).length === 0) {
+                        search = `?${queryString.stringify({ index })}`
+                    } else {
+                        search = `${this.props.location.search}&${queryString.stringify(index)}`
+                    }
+                    
+                    return (
+                        <Link
+                            key={index}
+                            to={`/page/${this.props.match.params.article}/${this.props.match.params.version}${search}`}
+                        >
+                            {chunk.text}
+                        </Link>
+                    )
                 })}
             </div>
         )
