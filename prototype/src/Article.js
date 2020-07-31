d694b1bb481d7aed4255cf6c5a4ff8d5e2edf134
SWKineo <spencer.ward@maine.edu>
2020-07-31 12:12:34 -0400

    Reworking expansions to make them more coherent

--- prototype/src/Article.js
+++ prototype/src/Article.js
@@ -10,8 +10,59 @@ export default class Article extends Component {
         super(props)
 
         this.state = {
-
+            expanded: queryString.parse(this.props.location.search)
         }
+    }
+
+    /**
+     * Renders an article body (an array of ArticleContents)
+     */
+    renderBody(body) {
+        return (
+            <div>
+                {body.map((chunk, index, _) => {
+                    let search = ""
+                    if (Object.keys(this.state.expanded).length === 0) {
+                        search = `?${queryString.stringify({ index })}`
+                    } else if (this.state.expanded.index === index ||
+                         (Array.isArray(this.state.expanded) &&
+                          this.state.expanded.includes(index))) {
+                        search = `${this.props.location.search}&${queryString.stringify(index)}`
+                    } else {
+                        search = this.props.location.search
+                    }
+                    if (chunk.expansion === undefined) {
+                        return (
+                            <p key={index}>{chunk.text}</p>
+                        )
+                    } else if (this.state.expanded.index === index ||
+                              (Array.isArray(this.state.expanded) &&
+                               this.state.expanded.includes(index))) {
+                        return (
+                            <div>
+                                <Link
+                                    key={index}
+                                    to={`/page/${this.props.match.params.article}/${this.props.match.params.version}${search}`}
+                                >
+                                    {chunk.text}
+                                </Link>
+                                {this.renderBody(chunk.expansion)}
+                            </div>
+                        )
+                    } else {
+                        return (
+                            <Link
+                                key={index}
+                                to={`/page/${this.props.match.params.article}/${this.props.match.params.version}${search}`}
+                            >
+                                {chunk.text}
+                            </Link>
+                        )
+                    }
+                    
+                })}
+            </div>
+        )
     }
 
     render() {
@@ -20,41 +71,43 @@ export default class Article extends Component {
                 <p>{sampleArticle.title}</p>
                 <p>{sampleArticle.formalism}</p>
                 <p>{sampleArticle.metaphor}</p>
-                {sampleArticle.body.map((chunk, index, _) => {
-                    let expanded = queryString.parse(this.props.location.search)
-
-                    if (chunk.expansion === undefined) {
-                        return <p key={index}>{chunk.text}</p>
-                    }
-
-                    if (expanded.index && (expanded === index || expanded.index.includes(index))) {
-                        return (
-                            <div>
-                                <p>{chunk.expansion}</p>
-                            </div>
-                        )
-                    }
-                    
-                    console.log(queryString.stringify({index}))
-                    console.log(Object.keys(expanded).length === 0)
-                    let search = ""
-                    if (Object.keys(expanded).length === 0) {
-                        search = `?${queryString.stringify({ index })}`
-                    } else {
-                        search = `${this.props.location.search}&${queryString.stringify(index)}`
-                    }
-                    
-                    return (
-                        <Link
-                            key={index}
-                            to={`/page/${this.props.match.params.article}/${this.props.match.params.version}${search}`}
-                        >
-                            {chunk.text}
-                        </Link>
-                    )
-                })}
+                {this.renderBody(sampleArticle.body)}
             </div>
         )
     }
 
 }
+
+// {sampleArticle.body.map((chunk, index, _) => {
+//     let expanded = queryString.parse(this.props.location.search)
+
+//     if (chunk.expansion === undefined) {
+//         return <p key={index}>{chunk.text}</p>
+//     }
+
+//     if (expanded.index && (expanded === index || expanded.index.includes(index))) {
+//         return (
+//             <div>
+//                 <p>{chunk.expansion.text}</p>
+//             </div>
+//         )
+//     }
+    
+//     console.log(queryString.stringify({index}))
+//     console.log(Object.keys(expanded).length === 0)
+//     let search = ""
+//     if (Object.keys(expanded).length === 0) {
+//         search = `?${queryString.stringify({ index })}`
+//     } else {
+//         search = `${this.props.location.search}&${queryString.stringify(index)}`
+//     }
+    
+//     return (
+//         <Link
+//             key={index}
+//             to={`/page/${this.props.match.params.article}/${this.props.match.params.version}${search}`}
+//         >
+//             {chunk.text}
+//         </Link>
+//     )
+// })}
--- prototype/src/Types/article.js
+++ prototype/src/Types/article.js
@@ -7,16 +7,16 @@ export default class ArticleAttempt {
         this.metaphor = "No metaphor"
         this.formalismMetaphorMapping = [ [1, 2, 3], [1, 2, 2] ]
         this.body = [
-            new AttemptContents("First set of words "),
-            new AttemptContents("expandable words", new AttemptContents("Expansion Text")),
-            new AttemptContents(" second set of words")
+            new AttemptContent("First set of words "),
+            new AttemptContent("expandable words", [ new AttemptContent("Expansion Text") ]),
+            new AttemptContent(" second set of words")
         ]
         this.theorems = []
     }
 }
 
 
-class AttemptContents {
+class AttemptContent {
 
     constructor(text, expansion) {
         this.text = text
